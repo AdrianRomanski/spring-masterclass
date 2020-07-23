a59e5a28de661ff77e334b18237c40cb283d7f3d
adrianromanski <adrianromanski@icloud.com>
2020-07-23 16:31:33 +0200

    Mapping Payment Entity to Hibernate

--- src/main/java/shop/payments/model/Payment.java
+++ src/main/java/shop/payments/model/Payment.java
@@ -1,17 +1,32 @@
 package shop.payments.model;
 
-import lombok.Builder;
-import lombok.Value;
+import lombok.*;
+import org.hibernate.annotations.Columns;
+import org.hibernate.annotations.TypeDef;
 import org.javamoney.moneta.FastMoney;
+import shop.common.FastMoneyUserType;
 
+import javax.persistence.*;
 import java.time.Instant;
 
-@Value
+@Data
+@Entity
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(exclude = "id")
+@Table(name = "payments", indexes = @Index(name = "payment_status", columnList = "status"))
+@TypeDef(name = "fastMoney", typeClass = FastMoneyUserType.class, defaultForType = FastMoney.class)
 public class Payment {
 
-    String id;
-    FastMoney money;
-    Instant timestamp;
-    PaymentStatus status;
+    @Id
+    private String id;
+    @Columns(columns = {
+            @Column(name = "currency", length = 3),
+            @Column(name = "value", length = 15)
+    })
+    private FastMoney money;
+    private Instant timestamp;
+    @Enumerated(EnumType.STRING)
+    private PaymentStatus status;
 }
